ab54b00d69adad8f9bcc17a9f4c09846aef7b0b4
AriManto <darkzoa10@gmail.com>
2024-09-25 23:22:16 -0300

    show results in html

--- Gauss/gauss.js
+++ Gauss/gauss.js
@@ -29,133 +29,138 @@ ruffini al polinomio con raicesEncontradasUnicas
 */
 console.log("HOLI");
 let polinomio = [];
-let boton = document.querySelector('button');
-let inputs = document.querySelectorAll('input');
+let boton = document.querySelector("button");
+let inputs = document.querySelectorAll("input");
 
 let TI = 1;
 let CP = 1;
-boton.addEventListener('click', function(e){
-    e.preventDefault();
-    polinomio.length = 0; //Clear del array
-    //if (validar()){
-        Gauss();
-    //} else {
-    // // Manejo de error
-    //}
-})
+boton.addEventListener("click", function (e) {
+  e.preventDefault();
+  polinomio.length = 0; //Clear del array
+  //if (validar()){
+  Gauss();
+  //} else {
+  // // Manejo de error
+  //}
+});
 
-
-function Gauss(){
-    class PosibleRaiz {
-        constructor(ti,cp){
-            this.TI = ti;
-            this.CP = cp;
-            this.fraccion = ti+"/"+cp;
-            this.valor = ti/cp;
-        }
-        mostrarFraccion(){
-            console.log(this.fraccion);
-        }
+function Gauss() {
+  class PosibleRaiz {
+    constructor(ti, cp) {
+      this.TI = ti;
+      this.CP = cp;
+      this.fraccion = ti + "/" + cp;
+      this.valor = ti / cp;
     }
-    function cargarPosiblesRaices(){
-        for (let i = 0; i < divisoresTI.length; i++){
-            for (let j = 0; j < divisoresCP.length; j++){
-                // console.log(divisoresTI[i]+"/"+divisoresCP[j]+" ")
-                posiblesRaices.push(new PosibleRaiz(divisoresTI[i],divisoresCP[j]));
-            }
-        }
+    mostrarFraccion() {
+      console.log(this.fraccion);
+      return this.fraccion;
     }
-    function mostrarPosiblesRaices(){
-        console.log("Posibles raíces:");
-        console.dir(posiblesRaices);
-        // posiblesRaices.forEach(function(raiz){
-        //     console.log(raiz.fraccion+" = "+raiz.valor);
-        //     // raiz.mostrarFraccion();
-        // });  
+  }
+  function cargarPosiblesRaices() {
+    for (let i = 0; i < divisoresTI.length; i++) {
+      for (let j = 0; j < divisoresCP.length; j++) {
+        // console.log(divisoresTI[i]+"/"+divisoresCP[j]+" ")
+        posiblesRaices.push(new PosibleRaiz(divisoresTI[i], divisoresCP[j]));
+      }
     }
-    function cargarDivisoresTI(){
-        //Normalización del signo
-        let TInorm = Math.pow(Math.pow(TI,2),1/2);
-        for (let i = TInorm; i >=0; i--){
-            if (TInorm % i == 0){
-                divisoresTI.push(parseInt(i));
-                divisoresTI.push(parseInt(-i));
-            }
-        }
-        console.log("Divisores TI : ");
-        console.dir(divisoresTI);
+  }
+  function mostrarPosiblesRaices() {
+    console.log("Posibles raíces:");
+    console.dir(posiblesRaices);
+    // posiblesRaices.forEach(function(raiz){
+    //     console.log(raiz.fraccion+" = "+raiz.valor);
+    //     // raiz.mostrarFraccion();
+    // });
+  }
+  function cargarDivisoresTI() {
+    //Normalización del signo
+    let TInorm = Math.pow(Math.pow(TI, 2), 1 / 2);
+    for (let i = TInorm; i >= 0; i--) {
+      if (TInorm % i == 0) {
+        divisoresTI.push(parseInt(i));
+        divisoresTI.push(parseInt(-i));
+      }
     }
-    function cargarDivisoresCP(){
-        //Normalización del signo
-        let CPnorm = Math.pow(Math.pow(CP,2),1/2);
-        for (let i = CPnorm; i >=0; i--){
-            if (CPnorm % i == 0){
-                divisoresCP.push(parseInt(i));
-                divisoresCP.push(parseInt(-i));
-
-            }
-        }
-        console.log("Divisores CP : ");
-        console.dir(divisoresCP);
+    console.log("Divisores TI : ");
+    console.dir(divisoresTI);
+  }
+  function cargarDivisoresCP() {
+    //Normalización del signo
+    let CPnorm = Math.pow(Math.pow(CP, 2), 1 / 2);
+    for (let i = CPnorm; i >= 0; i--) {
+      if (CPnorm % i == 0) {
+        divisoresCP.push(parseInt(i));
+        divisoresCP.push(parseInt(-i));
+      }
     }
-    function buscarRaices(){
-        for (let i=0; i<posiblesRaices.length; i++){
-            let evaluacion = 0;
-            for (let j=0; j<polinomio.length; j++){
-                // posibleraiz^i  (indice de polinomio)
-                //(polinomio[i].value * posible raiz)^i
-                evaluacion += polinomio[j] * Math.pow(posiblesRaices[i].valor,j);
-            } 
-            if (evaluacion==0){
-                // console.log(posiblesRaices[i].fraccion+" es raíz");
-                raicesEncontradas.push(posiblesRaices[i].valor);
-            }  
-        } 
-        raicesEncontradasUnicas = [ ...new Set(raicesEncontradas) ];
-        console.log("Raices encontradas:");
-        console.dir(raicesEncontradasUnicas);
+    console.log("Divisores CP : ");
+    console.dir(divisoresCP);
+  }
+  function buscarRaices() {
+    for (let i = 0; i < posiblesRaices.length; i++) {
+      let evaluacion = 0;
+      for (let j = 0; j < polinomio.length; j++) {
+        // posibleraiz^i  (indice de polinomio)
+        //(polinomio[i].value * posible raiz)^i
+        evaluacion += polinomio[j] * Math.pow(posiblesRaices[i].valor, j);
+      }
+      if (evaluacion == 0) {
+        // console.log(posiblesRaices[i].fraccion+" es raíz");
+        raicesEncontradas.push(posiblesRaices[i].valor);
+      }
     }
-    /* 
+    raicesEncontradasUnicas = [...new Set(raicesEncontradas)];
+    console.log("Raices encontradas:");
+    console.dir(raicesEncontradasUnicas);
+  }
+  function printResult(text) {
+    results.innerHTML += `<p>${text}<p>`;
+  }
+  function clearResult() {
+    results.innerHTML = "";
+  }
+  /* 
     //
     //-----------------MAIN-----------------------
     //
     */
-   let divisoresTI = [];
-   let divisoresCP = [];
-   let posiblesRaices = [];
-   let raicesEncontradas = [];
-   let raicesEncontradasUnicas = [];
+  let divisoresTI = [];
+  let divisoresCP = [];
+  let posiblesRaices = [];
+  let raicesEncontradas = [];
+  let raicesEncontradasUnicas = [];
+  let results = document.querySelector("#results");
 
-    //Cargar el array polinomio con los valores ingresados
-    for (let i = inputs.length-1; i>=0; i--){
-        polinomio.push(parseInt(inputs[i].value));
-    }
-    let polinomioParcial = polinomio;
-    TI = polinomio[0];
-    CP = polinomio[polinomio.length-1];
-    console.dir(polinomio);
-    console.log("CP: "+CP+" | TI: "+TI);
-    //Divisores de TI y CP
-    cargarDivisoresTI();
-    cargarDivisoresCP();
-    //Posibles raíces TI/CP
-    cargarPosiblesRaices();
-    mostrarPosiblesRaices();
+  //Cargar el array polinomio con los valores ingresados
+  for (let i = inputs.length - 1; i >= 0; i--) {
+    polinomio.push(parseInt(inputs[i].value));
+  }
+  let polinomioParcial = polinomio;
+  TI = polinomio[0];
+  CP = polinomio[polinomio.length - 1];
+  console.dir(polinomio);
+  console.log("CP: " + CP + " | TI: " + TI);
+  //Divisores de TI y CP
+  cargarDivisoresTI();
+  cargarDivisoresCP();
+  //Posibles raíces TI/CP
+  cargarPosiblesRaices();
+  mostrarPosiblesRaices();
 
-    buscarRaices();
-    if (raicesEncontradasUnicas.length > 0){
-        ////
-        console.log("Se encontraron raíces");
-        //Ruffini
-        //Mostrar resultado final
-    } else {
-        console.warn("No se encontraron raíces!!!!");
-    }
+  buscarRaices();
+  if (raicesEncontradasUnicas.length > 0) {
+    clearResult();
+    printResult("Se encontraron raíces =");
+    console.log("Se encontraron raíces");
+    raicesEncontradasUnicas.map((x) => printResult(x));
+    //Ruffini
+    //Mostrar resultado final
+  } else {
+    clearResult();
+    console.warn("No se encontraron raíces!!!!");
+    printResult("No se encontraron raíces");
+  }
 
-    
-
-
-
-
-    //----Fin de main----
+  //----Fin de main----
 }
