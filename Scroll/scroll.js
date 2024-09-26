437dc4d892646d3c9b0e91465a8a4684a890af80
AriManto <darkzoa10@gmail.com>
2024-09-25 23:47:13 -0300

    cleanup

--- Scroll/scroll.js
+++ Scroll/scroll.js
@@ -1,18 +1,18 @@
-let cards = document.querySelectorAll('.card');
-let c2 = document.querySelector('#c2');
-//
-//offsetTop
+let cards = document.querySelectorAll(".card");
+let c2 = document.querySelector("#c2");
 
-window.addEventListener('scroll', function(e){
-    let SCROLL = e.srcElement.scrollingElement;
-    console.log("Top: "+SCROLL.scrollTop);
-    cards.forEach(card => {
-        if(card.classList.contains('hiddenLeft') || card.classList.contains('hiddenRight')){
-            if(SCROLL.scrollTop + 400>=card.offsetTop){
-                card.classList.remove('hiddenLeft');
-                card.classList.remove('hiddenRight');
-                card.classList.add('shown');
-            };
-        };
-    });
+window.addEventListener("scroll", () => {
+  let SCROLL = document.scrollingElement;
+  cards.forEach((card) => {
+    if (
+      card.classList.contains("hiddenLeft") ||
+      card.classList.contains("hiddenRight")
+    ) {
+      if (SCROLL.scrollTop + 400 >= card.offsetTop) {
+        card.classList.remove("hiddenLeft");
+        card.classList.remove("hiddenRight");
+        card.classList.add("shown");
+      }
+    }
+  });
 });
